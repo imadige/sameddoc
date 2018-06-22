a58292780dca9225709047db260b0f2930b85cc7
samed <samed@se-kiz.com>
2018-06-22 15:03:52 +0300

    update

--- sunucu/extensionphp.php
+++ sunucu/extensionphp.php
@@ -1,4 +1,4 @@
 
 https://gist.github.com/jbanety/35d01689608ad89777d349f65c7b39c9 pcntl install
 
-sudo apt install php7.0-common php7.0-fpm php7.0-cli php7.0-json php7.0-mysql php7.0-curl php7.0-intl php7.0-mcrypt php-pear php7.0-gd php7.0-zip php7.0-xml php7.0-mbstring
+sudo apt install php7.0-soap php7.0-dev php7.0-common php7.0-fpm php7.0-cli php7.0-json php7.0-mysql php7.0-curl php7.0-intl php7.0-mcrypt php-pear php7.0-gd php7.0-zip php7.0-xml php7.0-mbstring
